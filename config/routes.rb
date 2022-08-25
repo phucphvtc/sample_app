3b640ba026f2bdc71f2597e1965453f9fb19d2f4
phucphvtc <phucphvtc@gmail.com>
2022-08-25 12:01:59 +0700

    AM 25/08

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/signup', to: 'user#new'
+  get 'static_pages/home', as: 'home'
+  # get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get 'static_pages/contact', as: 'contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   # Defines the root path route ("/")
