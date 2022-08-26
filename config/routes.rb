00bfc18120788ae833d7d4bfa09490567604a51a
phucphvtc <phucphvtc@gmail.com>
2022-08-26 14:18:26 +0700

    AM 26/8

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  
   get 'static_pages/home', as: 'home'
   # get 'static_pages/help'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get 'static_pages/contact', as: 'contact'
+
+  get '/signup', to: 'users#new'
+  get '/show', to:'users#show'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_103356) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_170608) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
