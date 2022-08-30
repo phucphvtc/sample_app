da57c6d4d62ddc703a20ddbfe23cfde3174c4c39
phucphvtc <phucphvtc@gmail.com>
2022-08-30 17:34:20 +0700

    PM 30/8

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_080513) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
@@ -28,7 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_080513) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
   end
 
