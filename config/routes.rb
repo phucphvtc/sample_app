ea5f7b66b8591b45c4e000f29b4cd5b85c69cfc5
phucphvtc <phucphvtc@gmail.com>
2022-09-13 09:41:58 +0700

    fix index users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get 'static_pages/contact', as: 'contact'
   #user
-  get '/signup', to: 'users#new'
+  # get '/signup', to: 'users#new'
   # get '/show', to:'users#show'
   # get '/index',to: 'users#index'
 
